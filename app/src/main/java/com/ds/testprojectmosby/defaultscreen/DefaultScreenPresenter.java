64f2591d427792dd0fd97df5e2bf9076d04dfb47
radulescu_serban <theodor.radu8@gmail.com>
2016-11-24 02:44:50 +0200

    random 2

--- app/src/main/java/com/ds/testprojectmosby/defaultscreen/DefaultScreenPresenter.java
+++ app/src/main/java/com/ds/testprojectmosby/defaultscreen/DefaultScreenPresenter.java
@@ -2,11 +2,12 @@ package com.ds.testprojectmosby.defaultscreen;
 
 import com.ds.testprojectmosby.EmptyView;
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
+import com.hannesdorfmann.mosby.mvp.MvpNullObjectBasePresenter;
 
 /**
  * Created by Serban Theodor on 17-Nov-16.
  */
 
-public class DefaultScreenPresenter extends MvpBasePresenter<EmptyView> {
+public class DefaultScreenPresenter extends MvpNullObjectBasePresenter<EmptyView> {
 
 }
